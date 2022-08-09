011dfe2ad2817b2ee9efe42c0a82d324770142ab
Fernanda <ma.munozleppez@gmail.com>
2022-08-08 21:58:04 -0400

    fix identation

--- src/E01NumerosDeMayorAMenor.java
+++ src/E01NumerosDeMayorAMenor.java
@@ -6,13 +6,10 @@ public class E01NumerosDeMayorAMenor {
         Scanner escaner = new Scanner(System.in);
         StringBuilder s = new StringBuilder();
 
-
-
         System.out.println("Ingrese numero 1");
         int numero1 = escaner.nextInt();
         System.out.println("Ingrese numero 2");
         int numero2 = escaner.nextInt();
-
 
         if(numero1 > numero2){
             System.out.println(s.append(numero1).append(" ").append(numero2));
