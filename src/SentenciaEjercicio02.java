b04686aaa6694dfaccdd6644e736390f3c78ddb3
osvaldoleiva <osvaldo.leiva@mercadolibre.cl>
2022-08-15 15:02:17 -0400

    fix ejercicio

--- src/SentenciaEjercicio02.java
+++ src/SentenciaEjercicio02.java
@@ -2,65 +2,55 @@ import java.util.Scanner;
 
 public class SentenciaEjercicio02 {
 
-
+    // Promedios < 4 Promedio > 5 Cantidad de nota 1 Promedio total
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         double[] notas = new double[4];
-        double promedioMa5 = 0.0;
-        double promedioMe4 = 0.0;
+        double promedioAprobado = 0.0;
+        double promedioReprobado = 0.0;
+        double acumuladoNotas = 0.0;
         int cantidadMa5 = 0;
         int cantidadMe4 = 0;
         int cantidadUnos = 0;
-        double promedio = 0.0;
-        int cantProm = 0;
         double promedioTotal = 0.0;
 
 
         for (int i = 0; i < 4; i++) {
             System.out.print("Ingrese la nota en la posición " + (i) + " :");
-            notas[i] = s.nextDouble();
-            if (notas[i] > 7.0) {
-                System.out.println("nota invalida");
-                break;
+            double temp = scanner.nextDouble();
+
+            while (!(temp <= 7.0 && temp > 0)) {
+                System.out.println("ingrese una nota valida");
+                temp = scanner.nextDouble();
             }
 
-        }
+            notas[i] = temp;
+            acumuladoNotas += temp;
 
-        for (int j = 0; j < notas.length; j++) {
-            if (notas[j] > 5.0) {
+            if (temp > 5.0) {
                 cantidadMa5++;
-                promedioMa5 += notas[j];
-                promedioMa5 = promedioMa5 / cantidadMa5;
+                promedioAprobado += temp;
+                promedioAprobado = promedioAprobado / cantidadMa5;
             }
-        }
-        for (int l = 0; l < notas.length; l++) {
-            if (notas[l] < 4.0) {
+            if (temp < 4.0) {
                 cantidadMe4++;
-                promedioMe4 += notas[l];
-                promedioMe4 = promedioMe4 / cantidadMe4;
+                promedioReprobado += temp;
+                promedioReprobado = promedioReprobado / cantidadMe4;
             }
-        }
-
-        for (int k = 0; k < notas.length; k++) {
-            if (notas[k] == 1.0) {
+            if (temp == 1.0) {
                 cantidadUnos++;
-
             }
         }
 
-        for (int g = 0; g < notas.length; g++) {
-            cantProm++;
-            promedio += notas[g];
-            promedioTotal = promedio / cantProm;
-        }
+        acumuladoNotas = acumuladoNotas / notas.length;
 
-
-            System.out.println("el promedio mayor a 5 es: " + promedioMa5 + " - ");
-            System.out.println("el promedio menor a 4 es: " + promedioMe4 + " - ");
-            System.out.println("Cantidad Unos: " + cantidadUnos + " - ");
-            System.out.println("el promedio total es: " + promedioTotal + " - ");
-            System.out.println("Fin");
+        System.out.println(acumuladoNotas);
+        System.out.println("el promedio mayor a 5 es: " + promedioAprobado + " - ");
+        System.out.println("el promedio menor a 4 es: " + promedioReprobado + " - ");
+        System.out.println("Cantidad Unos: " + cantidadUnos + " - ");
+        System.out.println("el promedio total es: " + promedioTotal + " - ");
+        System.out.println("Fin");
 
     }
 }
